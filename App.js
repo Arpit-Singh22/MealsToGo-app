ce34a3db7ccfe0d88ae789bcb1f519f8ec041057
Arpit-Singh22 <arpitsingh22@hotmail.com>
2023-05-09 20:26:24 +0530

    added notch margin

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Text,
+  View
+} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Search</Text>
+      </View>
+      <View style={styles.listContainer}>
+        <Text>List</Text>
+      </View>
+    </SafeAreaView>
+    <ExpoStatusBar style="auto" />
+  </>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
+  textContainer: {
+    flexDirection: 'column',
+    padding: 5,
+    backgroundColor: 'green',
+  },
+  listContainer: {
+    flex: 1,
+    padding: 5,
+    backgroundColor: 'blue',
+  }
 });
